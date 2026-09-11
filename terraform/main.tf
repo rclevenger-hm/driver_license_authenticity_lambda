6adfa9aa18322f2fd6dcdc6279b415eeec1fd6a5
rclevenger-hm <70983614+rclevenger-hm@users.noreply.github.com>
2026-09-11 09:11:04 -0500

    security: encrypt queue and durable state

--- terraform/main.tf
+++ terraform/main.tf
@@ -128,6 +128,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "intake_bucket_lifecycle" {
 resource "aws_sqs_queue" "screening_dlq" {
   name                      = "${local.queue_name}-dlq"
   message_retention_seconds = 1209600
+  sqs_managed_sse_enabled   = true
   tags                      = local.common_tags
 }
 
@@ -135,6 +136,7 @@ resource "aws_sqs_queue" "screening_jobs" {
   name                       = local.queue_name
   visibility_timeout_seconds = 120
   message_retention_seconds  = 345600
+  sqs_managed_sse_enabled    = true
   tags                       = local.common_tags
 
   redrive_policy = jsonencode({
@@ -148,6 +150,14 @@ resource "aws_dynamodb_table" "submissions" {
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "submissionId"
   tags         = local.common_tags
+
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  server_side_encryption {
+    enabled = true
+  }
 
   attribute {
     name = "submissionId"
